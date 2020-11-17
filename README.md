f2cc8806a0f1d68c362d3e43c4a1f3ba7aaa7043
Zein <m.fadli.zein.17@gmail.com>
2020-11-17 12:00:50 +0700

    README.md

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# MyLibSimpleSQLitekt
- https://github.com/gzeinnumer/MyLibSimpleSQLite
+# MyLibSQLiteExample
+
+This is the example from library [MyLibSQLite](https://github.com/gzeinnumer/MyLibSQLite).
+
+Run this Application on your `phone` or `emulator`, please give `permission` to this app , so it can read your `external`.
+
+but there is a step that you should do.. what i should do?
+
+1. Clone this repository
+
+2. Copy folder `MyLibSQLite` to your phone
+
+<p align="center">
+  <img src="https://github.com/gzeinnumer/MyLibSQLiteExample/blob/master/preview/example3.jpg" width="400"/>
+</p>
+
+3. Paste it like this
+
+<p align="center">
+  <img src="https://github.com/gzeinnumer/MyLibSQLiteExample/blob/master/preview/example2.jpg" width="400"/>
+</p>
+
+4. Here is my table that i use in this example app.
+
+<p align="center">
+  <img src="https://github.com/gzeinnumer/MyLibSQLiteExample/blob/master/preview/example1.JPG" width="400"/>
+</p>
+
+```
+Copyright 2020 M. Fadli Zein
+```
--- app/src/main/java/com/gzeinnumer/mylibsimplesqlitekt/TestActivity.kt
+++ app/src/main/java/com/gzeinnumer/mylibsimplesqlitekt/TestActivity.kt
@@ -16,24 +16,24 @@ class TestActivity : AppCompatActivity() {
 
         val table1 = Table1()
 
-//        val list: List<Table1> = table1.read()
-//        Log.d(TAG, "onCreate1: " + list[0].name)
-//        Log.d(TAG, "onCreate1: " + list.size)
-//
-//        val listQuery: List<Table1> = table1.query()
-//        Log.d(TAG, "onCreate2: " + listQuery[0].name)
-//        Log.d(TAG, "onCreate2: " + listQuery[0].table2_name)
+        val list: List<Table1> = table1.read()
+        Log.d(TAG, "onCreate1: " + list[0].name)
+        Log.d(TAG, "onCreate1: " + list.size)
 
-//        val istrue: Boolean = table1.insert(Table1(500, "Zein", 1.3, "Decs", 1, "2020-12-12"))
-//        Log.d(TAG, "onCreate3: $istrue")
-//
-//        val istrue2: Boolean = table1.update(Table1(500, "Zein", 1.6, "Decs", 1, "2020-11-12"))
-//        Log.d(TAG, "onCreate4: $istrue2")
-//
-//        val count: Int = table1.count()
-//        Log.d(TAG, "onCreate5: $count")
+        val listQuery: List<Table1> = table1.query()
+        Log.d(TAG, "onCreate2: " + listQuery[0].name)
+        Log.d(TAG, "onCreate2: " + listQuery[0].table2_name)
 
-//        val istrue3 = table1.delete()
-//        Log.d(TAG, "onCreate6: $istrue3")
+        val istrue: Boolean = table1.insert(Table1(500, "Zein", 1.3, "Decs", 1, "2020-12-12"))
+        Log.d(TAG, "onCreate3: $istrue")
+
+        val istrue2: Boolean = table1.update(Table1(500, "Zein", 1.6, "Decs", 1, "2020-11-12"))
+        Log.d(TAG, "onCreate4: $istrue2")
+
+        val count: Int = table1.count()
+        Log.d(TAG, "onCreate5: $count")
+
+        val istrue3 = table1.delete()
+        Log.d(TAG, "onCreate6: $istrue3")
     }
 }
