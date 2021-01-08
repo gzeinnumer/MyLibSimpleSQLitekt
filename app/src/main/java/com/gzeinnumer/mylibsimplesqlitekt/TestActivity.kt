7318c4d426d69b6fa9a7d13554e348f3da4eab03
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-01-08 19:29:35 +0700

    readme

--- app/src/main/java/com/gzeinnumer/mylibsimplesqlitekt/TestActivity.kt
+++ app/src/main/java/com/gzeinnumer/mylibsimplesqlitekt/TestActivity.kt
@@ -3,53 +3,67 @@ package com.gzeinnumer.mylibsimplesqlitekt
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.gzeinnumer.mylibsimplesqlitekt.databinding.ActivityTestBinding
 import com.gzeinnumer.mylibsimplesqlitekt.entity.Table1
 import com.gzeinnumer.mylibsimplesqlitekt.helper.GblVariabel
 
 class TestActivity : AppCompatActivity() {
     private  val TAG = "TestActivity"
+
+    private lateinit var binding: ActivityTestBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+        binding = ActivityTestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         GblVariabel.initDb(applicationContext)
 
         val table1 = Table1()
 
-        //Active 1 by 1 one if you want to test it
-
-//        val istrue = table1.insert()
-//        Log.d(TAG, "onCreate1: $istrue")
-
-//        val istrue2 = table1.update()
-//        Log.d(TAG, "onCreate2: $istrue2")
-
-//        val istrue3 = table1.delete()
-//        Log.d(TAG, "onCreate3: $istrue3")
-
-//        val count = table1.count()
-//        Log.d(TAG, "onCreate4: $count")
-
-//        val count2 = table1.count2()
-//        Log.d(TAG, "onCreate5: $count2")
-
-//        val count3 = table1.queryCount()
-//        Log.d(TAG, "onCreate6: $count3")
-
-//        val read = table1.read()
-//        Log.d(TAG, "onCreate7: " + read[0].name)
-//        Log.d(TAG, "onCreate7: " + read.size)
-
-//        val read2 = table1.read2()
-//        Log.d(TAG, "onCreate8: " + read2[0].name)
-//        Log.d(TAG, "onCreate8: " + read2.size)
-
-//        val listQuery = table1.query()
-//        Log.d(TAG, "onCreate9: " + listQuery[0].name)
-//        Log.d(TAG, "onCreate9: " + listQuery[0].table2_name)
-//        Log.d(TAG, "onCreate9: " + listQuery.size)
-
-//        val queryUpdate = table1.queryResultUpdate()
-//        Log.d(TAG, "onCreate10: $queryUpdate")
+        binding.btnInsert.setOnClickListener { view ->
+            val istrue = table1.insert()
+            Log.d(TAG, "onCreate_1: $istrue")
+        }
+        binding.btnUpdate.setOnClickListener { view ->
+            val istrue2 = table1.update()
+            Log.d(TAG, "onCreate_2: $istrue2")
+        }
+        binding.btnDelete.setOnClickListener { view ->
+            val istrue3 = table1.delete()
+            Log.d(TAG, "onCreate_3: $istrue3")
+        }
+        binding.btnCount.setOnClickListener { view ->
+            val count = table1.count()
+            Log.d(TAG, "onCreate_4: $count")
+        }
+        binding.btnCount2.setOnClickListener { view ->
+            val count2 = table1.count2()
+            Log.d(TAG, "onCreate_5: $count2")
+        }
+        binding.btnCount3.setOnClickListener { view ->
+            val count3 = table1.queryCount()
+            Log.d(TAG, "onCreate_6: $count3")
+        }
+        binding.btnRead.setOnClickListener { view ->
+            val read = table1.read()
+            Log.d(TAG, "onCreate_7: " + read[0].name)
+            Log.d(TAG, "onCreate_7: " + read.size)
+        }
+        binding.btnRead2.setOnClickListener { view ->
+            val read2 = table1.read2()
+            Log.d(TAG, "onCreate_8: " + read2[0].name)
+            Log.d(TAG, "onCreate_8: " + read2.size)
+        }
+        binding.btnQuery.setOnClickListener { view ->
+            val listQuery = table1.query()
+            Log.d(TAG, "onCreate_9: " + listQuery[0].name)
+            Log.d(TAG, "onCreate_9: " + listQuery[0].table2_name)
+            Log.d(TAG, "onCreate_9: " + listQuery.size)
+        }
+        binding.btnQueryResultUpdate.setOnClickListener { view ->
+            val queryUpdate = table1.queryResultUpdate()
+            Log.d(TAG, "onCreate_10: $queryUpdate")
+        }
     }
 }
